88795af86314d982981e4acf7e051e2e9d30cc09
joaoG <joaog.br35@usp.br>
2024-10-11 15:44:35 -0300

    some minor changes in generator

--- test_cases/generator/probabilityGen.py
+++ test_cases/generator/probabilityGen.py
@@ -1,42 +1,46 @@
 from partition_methods.relaxed_problem.python.graph import Graph
 import numpy as np
 import random as rand
+
 def distribution(graph : Graph, prec = 3):
+    
     exogenous, endogenous =  findExogen(graph)
     
-    cardinalities : list[int] = np.empty(len(exogenous), dtype= int)
-   
-    for i, k in enumerate(exogenous):
-        cardinalities[i] = graph.cardinalities[k] 
-    probs : np.array[np.array[float]] = np.empty(len(cardinalities), dtype = object)
-    print(cardinalities)
-    for j, card in enumerate(cardinalities):
-        dist = np.empty(card)
-        for i in range(dist.size):
-            dist[i] = rand.uniform(1,100)
-        dist = dist/np.sum(dist)
+    cardinalities : list[int] = [graph.cardinalities[i] for i in exogenous]
+
+    probs : list[np.array[float]] = []
+    
+    for card in cardinalities:
+        
+        dist = np.array([rand.uniform(1,100) for _ in range(card)])
+        
+        dist /= np.sum(dist)
         dist = np.trunc(dist*10**prec)/10**prec
         dist[-1] = 1 - np.sum(dist[0:card-1])        
-        probs[j] = dist
+        probs.append(dist)
 
     return probs
 
+#function that finds all exogenous indexs(implement in graph parser?)
 def findExogen(graph : Graph):
-    exogenIndex : list[int]  = []
+    
+    exogenIndex : list[int] = []
     endogenIndex : list[int] = []
+    
     for i in range(1, graph.num_nodes + 1):
         if not (bool(graph.parents[i])) :
            exogenIndex.append(i)
         else :
             endogenIndex.append(i)
+    
     return exogenIndex, endogenIndex
 
+#test function
 if __name__ == "__main__":
     graph: Graph = Graph.parse()
     for i in range(1, graph.num_nodes + 1):
         if graph.cardinalities[i] < 1:
             graph.cardinalities[i] = 2
-    print(graph.cardinalities, type(graph.cardinalities[1]))
     
     print(distribution(graph))
 
