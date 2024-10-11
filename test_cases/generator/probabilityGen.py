75c29a8663dc3fb383462bb5ebc1c3ecdff32e57
joaoG <joaog.br35@usp.br>
2024-10-11 14:35:57 -0300

    modified distribution generation, with  a function that computes all exogenous index(maybe, would be usefull to implement this as a parameter in class Graph)

--- test_cases/generator/probabilityGen.py
+++ test_cases/generator/probabilityGen.py
@@ -1,8 +1,15 @@
+from partition_methods.relaxed_problem.python.graph import Graph
 import numpy as np
 import random as rand
-def distribution(cardinalities : list[int], prec = 3):
-    probs = np.empty(len(cardinalities), dtype = object)
-
+def distribution(graph : Graph, prec = 3):
+    exogenous, endogenous =  findExogen(graph)
+    
+    cardinalities : list[int] = np.empty(len(exogenous), dtype= int)
+   
+    for i, k in enumerate(exogenous):
+        cardinalities[i] = graph.cardinalities[k] 
+    probs : np.array[np.array[float]] = np.empty(len(cardinalities), dtype = object)
+    print(cardinalities)
     for j, card in enumerate(cardinalities):
         dist = np.empty(card)
         for i in range(dist.size):
@@ -13,6 +20,26 @@ def distribution(cardinalities : list[int], prec = 3):
         probs[j] = dist
 
     return probs
+
+def findExogen(graph : Graph):
+    exogenIndex : list[int]  = []
+    endogenIndex : list[int] = []
+    for i in range(1, graph.num_nodes + 1):
+        if not (bool(graph.parents[i])) :
+           exogenIndex.append(i)
+        else :
+            endogenIndex.append(i)
+    return exogenIndex, endogenIndex
+
+if __name__ == "__main__":
+    graph: Graph = Graph.parse()
+    for i in range(1, graph.num_nodes + 1):
+        if graph.cardinalities[i] < 1:
+            graph.cardinalities[i] = 2
+    print(graph.cardinalities, type(graph.cardinalities[1]))
+    
+    print(distribution(graph))
+
 
 
         
