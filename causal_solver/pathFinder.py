707ed78bdfd98cb5a3f15754b80f79d6ad2cb606
joaoG <joaog.br35@usp.br>
2024-11-25 11:33:19 -0300

    Path finder

--- causal_solver/pathFinder.py
+++ causal_solver/pathFinder.py
@@ -6,49 +6,54 @@ def inCcomponent(graph : Graph, node : int):
                return graph.dag_components.index(cComponent)
      return None
 #Encontra o caminho de c-Components entre os nós
-def findPath_it(nodes : list[int], graph : Graph, possiblePath : set[tuple] = set([])):
+def findPath_it(nodes : list[int], graph : Graph, possiblePath : set[tuple] = set()):
     # dest | do(src) 
-    src : int = nodes[0]
-    dest : int = nodes[1]
-    path : set[tuple] = set([])
-    possiblePath.add((inCcomponent(node=dest, graph= graph),dest))
-    if dest == src :
-     return  possiblePath
-    
-    if not(graph.parents[dest]):
-     return {}
-    
-    graph.curr_nodes.append(dest)
-    
-    for parent in graph.parents[dest]: 
-     
-     path.update(findPath_it(nodes= [src,parent], graph= graph, possiblePath= possiblePath.copy()))   
-    
-    return path
+     src : int = nodes[0]
+     dest : int = nodes[1]
+
+     if not(dest in graph.curr_nodes):
+          graph.curr_nodes.append(dest)
+                    
+          path : set[tuple] = set()
+          possiblePath.add((inCcomponent(node=dest, graph= graph),dest))          
+          if dest == src :
+               return  possiblePath
+                    
+          if not(graph.parents[dest]):
+               return {}
+                    
+          for parent in graph.parents[dest]: 
+                         
+               path.update(findPath_it(nodes= [src,parent], graph= graph, possiblePath= possiblePath.copy()))   
+                    
+          return path
+     return possiblePath
 
 def findPath(nodes : list[str], graph : Graph):
+     
      src = graph.label_to_index[nodes[0]]
      dest = graph.label_to_index[nodes[1]]
      graph.curr_nodes.clear()
      path = findPath_it(nodes=[src, dest], graph=graph)
+     graph.curr_nodes.clear()
      componentsPath : list[int] = []
      nodesPath : list[int] = []
-     print(path)
+
      for pair in path:
         if not(pair[0] in componentsPath):
           componentsPath.append(pair[0])
-        nodesPath.append(graph.index_to_label[pair[1]])
+        
+        if not(pair[1] in nodesPath): 
+          nodesPath.append(graph.index_to_label[pair[1]])
      
-     graph.curr_nodes.clear()
 
      return componentsPath, nodesPath
 
 if __name__ == "__main__":
     graph: Graph = Graph.parse()
     graph.find_cComponents()
-    graph.curr_nodes.clear()
-    nodes= ["E", "Y"]
+    nodes= ["Y", "Y"]
     componentsPath, nodesPath = findPath(nodes= nodes, graph= graph)
-    print(not(graph.parents[5]))
+    
     print(f"Path of c-components: {componentsPath} \n")
     print(f"Path of nodes: {nodesPath}")
