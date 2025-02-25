705065dffe3161d79384cfcc31294f28f1e9101b
JoaoArroyo004 <comp.arroyo@gmail.com>
2025-02-25 16:01:03 -0300

    improve debugging: prints the symbolic expression of the objective function & store the involved variables (in order)

--- linear_algorithm/restrictions_generator.py
+++ linear_algorithm/restrictions_generator.py
@@ -6,7 +6,10 @@ class restrictions_generator:
     a linear objective function.
     """
     
-    def __init__(self, graph: Graph, intervention: int | str, target: int | str):
+    def __init__(self, graph: Graph, intervention: int | str, target: int | str,
+                empiricalProbabilitiesVariables: list[int], mechanismVariables: list[int],  
+                conditionalProbabilitiesVariables: list[tuple[int, list[int]]],
+                debugOrder: list[int]):
         """
         graph: an instance of the personalized class graph
         intervention: X in P(Y|do(X))
@@ -16,6 +19,11 @@ class restrictions_generator:
         self.graph = graph
         self.intervention = intervention
         self.target = target
+
+        self.empiricalProbabilitiesVariables = empiricalProbabilitiesVariables
+        self.mechanismVariables = mechanismVariables
+        self.conditionalProbabilities = conditionalProbabilitiesVariables
+        self.debugOrder = debugOrder
     
     def generate_restrictions(self):
         """
@@ -27,6 +35,7 @@ class restrictions_generator:
         empiricalProbabilitiesVariables = [] # If V in this array then it implies P(v) in the objective function
         mechanismVariables = []              # If V in this array then it implies a decision function: 1(Pa(v) => v= some value)
         conditionalProbabilities: list[tuple[int, list[int]]] = []        # If V|A,B,C in this array then it implies P(V|A,B,C) in the objective function
+        debugOrder: list[int] = []
         
         while len(current_targets) > 0:
             print("---- Current targets array:")
@@ -40,13 +49,14 @@ class restrictions_generator:
                     current_target = node                            
             
             current_targets.remove(current_target)                       
-            print(f"Current target: {current_target}")
+            debugOrder.append(current_target)
+            print(f"Current target: {current_target}")            
 
             if not self.graph.is_descendant(ancestor=self.intervention, descendant=current_target):
-                    print(f"------- Case 1: Not a descendant") # OK!
+                    print(f"------- Case 1: Not a descendant")
                     empiricalProbabilitiesVariables.append(current_target)
             elif self.graph.graphNodes[current_target].latentParent == interventionLatent:
-                    print(f"------- Case 2: Mechanisms") # OK!
+                    print(f"------- Case 2: Mechanisms")
                     mechanismVariables.append(current_target)
                     for parent in self.graph.graphNodes[current_target].parents:
                         if (parent not in current_targets) and parent != intervention:
@@ -55,6 +65,7 @@ class restrictions_generator:
                     print(f"------- Case 3: Find d-separator set")
                     ancestors = self.graph.find_ancestors(node=current_target)
                     conditionableAncestors: list[int] = []
+                    
                     for ancestor in ancestors:
                         # Question: does it need to not be the intervention?
                         if self.graph.cardinalities[ancestor] > 0 and ancestor != current_target: 
@@ -86,16 +97,29 @@ class restrictions_generator:
 
                             current_targets = list((set(current_targets) | set(conditionedNodes)) - {intervention, current_target})
                         
-                        # Choose one of the valid subsets - Last instance of "separator", for now
-                        conditionalProbabilities.append((current_target, conditionedNodes))
+                    # Choose one of the valid subsets - Last instance of "separator", for now.
+                    conditionalProbabilities.append((current_target, separator))
                         
                         
                         # Question: is any already solved variable selected for the second time? Does the program need to address this issue
                         # by forcing the set to not contain any of such variables?
-
-        
-        # TODO: implement the objective function generator.
+        self.empiricalProbabilitiesVariables = empiricalProbabilitiesVariables
+        self.mechanismVariables = mechanismVariables
+        self.conditionalProbabilities = conditionalProbabilities          
+        self.debugOrder = debugOrder
         print("Test completed")
+    
+    def build_objective_function(self):
+         """
+         Intermediate step: remove useless endogenous variables in the mechanisms creation? 
+         Must be called after generate restrictions. Returns the objective function with the following encoding:
+         ....
+         """
+
+         # (2) Determine what are the variables that should appear in the iteration: generate
+
+         # (1) Call mechanism generator
+         pass
 
     def test():
         """
@@ -111,8 +135,32 @@ class restrictions_generator:
             print(f"parents: {graph.graphNodes[i].parents}")            
         print("\n\n\n\n")
 
-        rg = restrictions_generator(graph=graph, intervention=graph.labelToIndex["X"], target=graph.labelToIndex["Y"])
+        rg = restrictions_generator(graph=graph, intervention=graph.labelToIndex["X"], target=graph.labelToIndex["Y"],
+                                    empiricalProbabilitiesVariables=[], mechanismVariables=[], conditionalProbabilitiesVariables=[],
+                                    debugOrder=[])
         rg.generate_restrictions()
+        print(f"\n\n-------- Debug restrictions --------")
+        for node in rg.debugOrder:            
+            if node in rg.empiricalProbabilitiesVariables:
+                 print(f"P({rg.graph.indexToLabel[node]})", end="")
+            elif node in rg.mechanismVariables:
+                 parents: str = ""
+                 for parent in rg.graph.graphNodes[node].parents:
+                      parents += f"{rg.graph.indexToLabel[parent]}, "
+                 print(f"P({rg.graph.indexToLabel[node]}|{parents[:-2]})", end="")
+            else:
+                i = 0
+                while (rg.conditionalProbabilities[i][0] != node):                     
+                     i += 1
+                     
+                wset: str = ""
+                for condVar in rg.conditionalProbabilities[i][1]:
+                    if  condVar != rg.intervention:
+                        wset += f"{rg.graph.indexToLabel[condVar]}, "
+                print(f"P({rg.graph.indexToLabel[node]}|{rg.graph.indexToLabel[rg.intervention]}, {wset[:-2]})", end="")
 
+            if (node != rg.debugOrder[-1]): print(" * ", end="")
+
+        print("\n")        
 if __name__ == "__main__":
     restrictions_generator.test()
