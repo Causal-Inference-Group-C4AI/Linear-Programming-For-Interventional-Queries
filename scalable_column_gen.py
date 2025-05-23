f0089173b919af69f4519929012a4456cce3dc95
Daniel Lawand <daniel.lawand@gmail.com>
2025-05-23 16:16:53 -0300

    udpate

--- scalable_column_gen.py
+++ scalable_column_gen.py
@@ -5,21 +5,21 @@ from causal_reasoning.utils.get_scalable_df import getScalableDataFrame
 import time as tm
 
 def main():
-    # df = pd.DataFrame(columns=['N','M','LOWER_BOUND','LOWER_BOUND_SECONDS_TAKEN','LOWER_BOUND_REQUIRED_ITERATIONS','UPPER_BOUND','UPPER_BOUND_SECONDS_TAKEN','UPPER_BOUND_REQUIRED_ITERATIONS','BOUNDS_SIZE', 'TRUE_PROBABILITY_VALUE'])
-    # df.to_csv("./outputs/results.csv", index=False)
+    df = pd.DataFrame(columns=['N','M','LOWER_BOUND','LOWER_BOUND_REQUIRED_ITERATIONS','UPPER_BOUND','TOTAL_SECONDS_TAKEN','UPPER_BOUND_REQUIRED_ITERATIONS','BOUNDS_SIZE'])
+    df.to_csv("./outputs/results_1.csv", index=False)
     N_M = [
-            (1,1),
-            (2,1),
-            (3,1),
-            (4,1),
-            (5,1),
-            (1,2),
-            (2,2),
-            (3,2),
-            (4,2),
-            (1,3),
-            (2,3),
-            (3,3)]
+            #(1,1),
+            #(2,1),
+            #(3,1),
+            #(4,1),
+            #(5,1),
+            #(1,2),
+            #(2,2),
+            (3,2)]#,
+            #(4,2),
+            #(1,3),
+            #(2,3),
+            #(3,3)]
     for values in N_M:
         N, M = values
         try:
@@ -28,11 +28,8 @@ def main():
             start = tm.time()
             scalarProblem = ScalarProblem.buildScalarProblem(M=M, N=N, interventionValue=interventionValue, targetValue=targetValue, df=scalable_df, minimum = True)
             lower , lower_iterations = scalarProblem.solve()
-            end = tm.time()
-            lower_time = end-start
 
             scalarProblem = ScalarProblem.buildScalarProblem(M=M, N=N, interventionValue=interventionValue, targetValue=targetValue, df=scalable_df, minimum = False)
-            start = tm.time()
             upper, upper_iterations = scalarProblem.solve()
             end = tm.time()
             upper = -upper
@@ -42,11 +39,11 @@ def main():
                 bounds_size = upper - lower
             else:
                 bounds_size = None
-            df = pd.read_csv("./outputs/results.csv")
-            new_row = {'N': N,'M': M,'LOWER_BOUND': lower,'LOWER_BOUND_SECONDS_TAKEN': lower_time,'LOWER_BOUND_REQUIRED_ITERATIONS': lower_iterations,'UPPER_BOUND': upper,'UPPER_BOUND_SECONDS_TAKEN': upper_time,'UPPER_BOUND_REQUIRED_ITERATIONS': upper_iterations,'BOUNDS_SIZE': bounds_size, 'TRUE_PROBABILITY_VALUE':checkQuerry(N,M,1,1)}
+            df = pd.read_csv("./outputs/results_1.csv")
+            new_row = {'N': N,'M': M,'LOWER_BOUND': lower,'LOWER_BOUND_REQUIRED_ITERATIONS': lower_iterations,'UPPER_BOUND': upper,'TOTAL_SECONDS_TAKEN': upper_time,'UPPER_BOUND_REQUIRED_ITERATIONS': upper_iterations,'BOUNDS_SIZE': bounds_size}
             new_row_df = pd.DataFrame([new_row])
             df = pd.concat([df, new_row_df], ignore_index=True)
-            df.to_csv("./outputs/results.csv", index=False)
+            df.to_csv("./outputs/results_1.csv", index=False)
         except Exception:
             pass
     print("Done")
